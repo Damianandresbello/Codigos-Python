ad118a882fd4a338cbe37c45465cc20aaa1a454b
Damianandresbello <49068577+Damianandresbello@users.noreply.github.com>
2019-04-05 12:00:25 -0300

    codigos_python
    algunos codigos 05/04/19

---
+++ introcivil/Codigos python.py
@@ -0,0 +1,20 @@
+N=int(input("Ingresar Numero: "))
+10
+i=1
+
+while(i<=N):
+if(i%2 !=0)
+print()
+i=i+1
+if(i==y):
+print("Iguales")
+n=0
+N=int(input("Ingresar Numero: "))
+if (N) in range (0,20):
+    print("Esta en el rango :D ")
+else:
+    print("No esta en el rango D: ")
+
+
+
+    
